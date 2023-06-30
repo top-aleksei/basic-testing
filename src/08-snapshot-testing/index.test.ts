e837caf2b2fc7a25434e08531edf4bee3298ad3d
Aleksei <toporov.aleksei@gmail.com>
2023-07-01 02:03:05 +0300

    feat: 8th module

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,33 @@
 // Uncomment the code below and write your tests
 // import { generateLinkedList } from './index';
 
+import { generateLinkedList } from '08-snapshot-testing';
+
 describe('generateLinkedList', () => {
+  const testElements = [1, 2, 3, 4];
+  const testLinkedList = {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: {
+            value: null,
+            next: null,
+          },
+        },
+      },
+    },
+  };
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList(testElements)).toStrictEqual(testLinkedList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList(testElements)).toMatchSnapshot();
   });
 });
